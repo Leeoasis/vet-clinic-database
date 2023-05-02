3927b94686e466c046e607c48db7a1ef5048e931
Leslie <Kooliesk@gmail.com>
2023-05-02 22:06:46 +0200

    add transactions in the queries.sql file

--- queries.sql
+++ queries.sql
@@ -9,6 +9,74 @@ SELECT * FROM animals WHERE neutered;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
+/*Transactions*/
+
+BEGIN TRANSACTION;
+
+UPDATE animals
+SET species = 'unspecified';
+
+SELECT * FROM animals;
+
+ROLLBACK;
+
+SELECT * FROM animals;
+
+
+
+BEGIN TRANSACTION;
+
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+
+COMMIT;
+
+
+BEGIN TRANSACTION;
+
+DELETE FROM animals;
+
+-- Verify that all rows have been deleted
+SELECT * FROM animals;
+
+ROLLBACK;
+
+-- Verify that the deletion has been rolled back
+SELECT * FROM animals;
+
+
+
+BEGIN TRANSACTION;
+
+-- Delete all animals born after Jan 1st, 2022
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+
+-- Create a savepoint
+SAVEPOINT my_savepoint;
+
+-- Update all animals' weight to be their weight multiplied by -1
+UPDATE animals
+SET weight_kg = -1 * weight_kg;
+
+-- Rollback to the savepoint
+ROLLBACK TO my_savepoint;
+
+-- Update all animals' weights that are negative to be their weight multiplied by -1
+UPDATE animals
+SET weight_kg = -1 * weight_kg
+WHERE weight_kg < 0;
+
+-- Commit the transaction
+COMMIT;
+
+SELECT * FROM animals;
+
 /*Udate table queries*/
 
 SELECT COUNT(*) FROM animals;
--- schema.sql
+++ schema.sql
@@ -15,71 +15,7 @@ ADD species VARCHAR(255);
 
 
 
-BEGIN TRANSACTION;
 
-UPDATE animals
-SET species = 'unspecified';
-
-SELECT * FROM animals;
-
-ROLLBACK;
-
-SELECT * FROM animals;
-
-
-
-BEGIN TRANSACTION;
-
-UPDATE animals
-SET species = 'digimon'
-WHERE name LIKE '%mon';
-
-UPDATE animals
-SET species = 'pokemon'
-WHERE species IS NULL;
-
-COMMIT;
-
-
-BEGIN TRANSACTION;
-
-DELETE FROM animals;
-
--- Verify that all rows have been deleted
-SELECT * FROM animals;
-
-ROLLBACK;
-
--- Verify that the deletion has been rolled back
-SELECT * FROM animals;
-
-
-
-BEGIN TRANSACTION;
-
--- Delete all animals born after Jan 1st, 2022
-DELETE FROM animals
-WHERE date_of_birth > '2022-01-01';
-
--- Create a savepoint
-SAVEPOINT my_savepoint;
-
--- Update all animals' weight to be their weight multiplied by -1
-UPDATE animals
-SET weight_kg = -1 * weight_kg;
-
--- Rollback to the savepoint
-ROLLBACK TO my_savepoint;
-
--- Update all animals' weights that are negative to be their weight multiplied by -1
-UPDATE animals
-SET weight_kg = -1 * weight_kg
-WHERE weight_kg < 0;
-
--- Commit the transaction
-COMMIT;
-
-SELECT * FROM animals;
 
 
 
