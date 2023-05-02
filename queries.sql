ec80a95a58855d45d24643d735ad6f00eb885ce2
Leslie <Kooliesk@gmail.com>
2023-05-02 20:10:01 +0200

    query and update animals table

--- queries.sql
+++ queries.sql
@@ -9,4 +9,20 @@ SELECT * FROM animals WHERE neutered;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
+/*Udate table queries*/
 
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight) FROM animals;
+SELECT neutered, SUM(escape_attempts) AS total_escape_attempts
+FROM animals
+GROUP BY neutered
+ORDER BY total_escape_attempts DESC
+LIMIT 1;
+SELECT species, MIN(weight) AS min_weight, MAX(weight) AS max_weight
+FROM animals
+GROUP BY species;
+SELECT species, AVG(escape_attempts) AS avg_escape_attempts
+FROM animals
+WHERE birthdate BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
--- schema.sql
+++ schema.sql
@@ -6,5 +6,88 @@ CREATE TABLE animals (
     date_of_birth DATE,
     escape_attempts INTEGER,
     neutered BOOLEAN,
-    weight_kg DECIMAL
+    weight_kg DECIMAL,
 );
+
+
+ALTER TABLE animals
+ADD species VARCHAR(255);
+
+
+
+BEGIN TRANSACTION;
+
+UPDATE animals
+SET species = 'unspecified';
+
+SELECT * FROM animals;
+
+ROLLBACK;
+
+SELECT * FROM animals;
+
+
+
+BEGIN TRANSACTION;
+
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+
+COMMIT;
+
+
+BEGIN TRANSACTION;
+
+DELETE FROM animals;
+
+-- Verify that all rows have been deleted
+SELECT * FROM animals;
+
+ROLLBACK;
+
+-- Verify that the deletion has been rolled back
+SELECT * FROM animals;
+
+
+
+BEGIN TRANSACTION;
+
+-- Delete all animals born after Jan 1st, 2022
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+
+-- Create a savepoint
+SAVEPOINT my_savepoint;
+
+-- Update all animals' weight to be their weight multiplied by -1
+UPDATE animals
+SET weight_kg = -1 * weight_kg;
+
+SELECT * FROM animals;
+
+-- Rollback to the savepoint
+ROLLBACK TO my_savepoint;
+
+SELECT * FROM animals;
+
+
+-- Update all animals' weights that are negative to be their weight multiplied by -1
+UPDATE animals
+SET weight_kg = -1 * weight_kg
+WHERE weight_kg < 0;
+
+-- Commit the transaction
+COMMIT;
+
+SELECT * FROM animals;
+
+
+
+
+
+
