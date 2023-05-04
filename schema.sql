6cfff38c5ef8bdcfc64f1c42a6426d90f28c44b8
Leslie <Kooliesk@gmail.com>
2023-05-04 12:21:42 +0200

    query multiple tables

--- schema.sql
+++ schema.sql
@@ -13,6 +13,24 @@ CREATE TABLE animals (
 ALTER TABLE animals
 ADD species VARCHAR(255);
 
+.
+
+/* Create a table named owners */
+
+CREATE TABLE owners (
+   id SERIAL PRIMARY KEY,
+   full_name varchar(255) NOT NULL,
+   age int NOT NULL
+);
+
+/* Create a table named species */
+CREATE TABLE species (
+   id SERIAL PRIMARY KEY,
+   name varchar(255) NOT NULL
+);
+
+/* Modify animals table */
+
 
 
 
